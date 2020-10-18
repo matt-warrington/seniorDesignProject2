8daa57ab853b6674532b37a44d0de75d46d107e4
matt-warrington <57462367+matt-warrington@users.noreply.github.com>
2020-10-18 18:00:39 -0400

    Update background_subtraction.m
    This is now the main file

--- background_subtraction.m
+++ background_subtraction.m
@@ -2,9 +2,30 @@
 clear all
 close all
 clc
-%% Read in Original and Background Images
+
+%% Check Camera List (may have to install MATLAB package)
+% cam_list = webcamlist;
+% cam_name = cam_list{2}; % Change index to 1 to use your laptop's webcam
+% cam = webcam(cam_name);
+% preview(cam)
+% 
+% %% Take Snapshot
+% x = input('Hit enter to take original image\n');
+% closePreview(cam)
+% [Image_Orig, img] = snapshot(cam);
+% preview(cam)
+% 
+% %% Take another snapshot
+% x = input('Hit enter to take original image\n');
+% closePreview(cam)
+% [Image_Background, img2] = snapshot(cam);
+
 Image_Orig = imread('StickersWithNoise.png');
 Image_Background = imread('NoiseBackground.png');
+
+%% Read in Original and Background Images
+%Image_Orig = imread('StickersWithNoise.png');
+%Image_Background = imread('NoiseBackground.png');
 [height,width,depth] = size(Image_Orig);
 [height_b,width_b,depth_b] = size(Image_Background);
 
@@ -30,7 +51,11 @@ Image_Binary = im2bw(Image_BackgroundSub);
 figure();
 imshow(Image_Binary);
 title('Binary Background Subtraction Image (Broken)');
-% Check the image, notice blue stickers have ben classified as background
+
+[centers, radii, metric] = imfindcircles(Image_Binary, [5 35]);
+
+viscircles(centers, radii, 'EdgeColor', 'b')
+
 
 %% Fix Image to Compute Binary Image
 Image_BackgroundSub2 = Image_BackgroundSub;
@@ -57,7 +82,7 @@ title('Binary Background Subtraction Image (Fixed)');
 
 %% Erode Image
 % Set Morphological Operation
-SE = strel('disk',20);
+SE = strel('disk',5);
 
 Image_Erode = imerode(Image_Binary2, SE);
 figure();
@@ -66,7 +91,7 @@ title('Binary Image Erosion');
 
 %% Dilate Image
 % Set Morphological Operation
-SE = strel('disk',20);
+SE = strel('disk',5);
 
 Image_Dilate = imdilate(Image_Binary2, SE);
 figure();
@@ -74,23 +99,19 @@ imshow(Image_Dilate);
 title('Binary Image Dilate');
 
 %% Call regionprops 
-STATS = regionprops(Image_Binary2, 'all')
-
-%% Print overlay
-figure();
-imshow(Image_Orig)
-hold on;
-
-items = size(STATS);
-for i = 1:items
-    plot(STATS(i).Centroid(1),STATS(i).Centroid(2),'kO','MarkerFaceColor','k');
-end
-
-title('Original Image with Centroid Dots');
-
-
-
-
+% STATS = regionprops(imcomplement(Image_Binary2), 'all')
+% 
+% %% Print overlay
+% figure();
+% imshow(Image_Orig)
+% hold on;
+% 
+% items = size(STATS);
+% for i = 1:items
+%     plot(STATS(i).Centroid(1),STATS(i).Centroid(2),'kO','MarkerFaceColor','k');
+% end
+% 
+% title('Original Image with Centroid Dots');
 
 
 
